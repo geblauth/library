f28eaebc0b766970a9234370ba5feda514faf00c
geblauth <54649083+geblauth@users.noreply.github.com>
2024-04-21 19:46:03 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
-import express from "express";
-import conectaDB from "./config/dbConnect.js";
-import routes from "./routes/index.js";
+import express from 'express'
+import conectaDB from './config/dbConnect.js'
+import routes from './routes/index.js'
+import manipuladorErros from './middleware/manipuladorErros.js'
+import manipulador404 from './middleware/manipulador404.js'
 
-const conexao = await conectaDB();
+const conexao = await conectaDB()
 
-conexao.on("error", (erro) => {
-    console.error("Erro de conexao:", erro);
+conexao.on('error', (erro) => {
+  console.error('Erro de conexao:', erro)
 })
 
-conexao.once("open", ()=>{
-    console.log("Conexao feita com sucesso!");
+conexao.once('open', () => {
+  console.log('Conexao feita com sucesso!')
 })
 
-const app = express();
+const app = express()
 
-routes(app);
+routes(app)
 
-app.delete("/livros/:id",(req, res)=>{
-    const index = buscaLivro(req.params.id);
-    livros.splice(index,1);
-    res.status(200).send("Livro Deletado!");
+app.use(manipulador404)
 
+app.use((erro, req, res, next) => {
+  manipuladorErros(erro, req, res, next)
 })
 
-
-export default app;
+export default app
